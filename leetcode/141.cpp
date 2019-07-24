817798ea80906ed8b72a4b8dd73700594575cdf4
chill-cy <2547116322>
2019-07-24 15:01:27 +0800

    updata

--- leetcode/141.cpp
+++ leetcode/141.cpp
@@ -2,12 +2,20 @@
 	> File Name: 141.cpp
 	> Author:jiangxiaoyu 
 	> Mail:2291372880@qq.com 
-	> Created Time: 2019年07月20日 星期六 15时57分51秒
+	> Created Time: 2019年07月10日 星期三 22时46分39秒
  ************************************************************************/
-
-//判断链表是否ｃｕｎ
 
 #include<iostream>
 using namespace std;
 
-
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *p = head, *q = head;
+    if (p == NULL) return false;
+    do {
+        p = p->next;
+        q = q->next;
+        if (q == NULL || q->next == NULL) return false;
+        q = q->next;
+    }
+    return true;
+}
