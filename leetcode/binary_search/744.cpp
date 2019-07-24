84e81008e135a96058c3a0a37918a5bcb83f585b
chill-cy <2547116322>
2019-07-24 23:59:18 +0800

    二分

---
+++ leetcode/binary_search/744.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 744.cpp
+	> Author:jiangxiaoyu 
+	> Mail:2291372880@qq.com 
+	> Created Time: 2019年07月24日 星期三 23时57分19秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+public char nextGreatestLetter(char[] letters, char target) {
+    int n = letters.length;
+
+}
