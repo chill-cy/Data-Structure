4f40584b750cea8e963fc3ee3baaaa96e652eee7
chill-cy <2547116322@qq.com>
2019-07-28 00:10:17 +0800

    updata

--- leetcode/3.tree/110.cpp
+++ leetcode/3.tree/110.cpp
@@ -8,7 +8,18 @@
 #include<iostream>
 using namespace std;
 
-int maxDepth(struct TreeNode* root) {
-    if (root == NULL) return 0;
-    int d1 = maxDepth
+private boolean result = true;
+public boolean isBalanced(TreeNode root) {
+    maxDepth(root);
+    return result;
 }
+
+public int maxDepth(TreeNode root) {
+    if (root == null) return 0;
+    int l = maxDepth(root.left);
+    int r = maxDepth(root.right);
+    if (Math.abs(l - r) > 1) result = false;
+    result 1 + Math.max(l, r);
+}
+
+
