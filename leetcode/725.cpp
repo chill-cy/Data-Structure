c80acf50e1136888a10b8ad52eef0855b7e5afdc
chill-cy <2547116322@qq.com>
2019-08-04 23:59:19 +0800

    updata

---
+++ leetcode/725.cpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+	> File Name: 725.cpp
+	> Author:jiangxiaoyu 
+	> Mail:2291372880@qq.com 
+	> Created Time: 2019年08月04日 星期日 23时58分51秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+public ListNode[] splitListToParts(ListNode root, int k) {
+    int N = 0;
+    ListNode cur = root;
+    while (cur != null) {
+        N++;
+        cur = cur.next;
+    }
+    int mod = N % k;
+    int size = N / k;
+    ListNode[] ret = new ListNode[k];
+    cur = root;
+    for (int i = 0; cur != null && i < k; i++) {
+        ret[i] = cur;
+        int curSize = size + (mod-- > 0 ? 1 : 0);
+        for (int j = 0; j < curSize - 1; j++) {
+            cur = cur.next;
+        }
+        ListNode next = cur.next;
+        cur.next = null;
+        cur = next;
+    }
+    return ret;
+}
