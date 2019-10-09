34283120b6519247d89b0e49008ef9005b273495
chill-cy <2547116322@qq.com>
2019-10-09 20:56:30 +0800

    updata

---
+++ 2.cpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+	> File Name: 2.cpp
+	> Author:jiangxiaoyu 
+	> Mail:2291372880@qq.com 
+	> Created Time: 2019年10月09日 星期三 20时56分16秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+Blocking
+
+    1. Blocking 
+
+    2. Starvation-Free
+
+Obstruction-Free
+
+    3. Obstruction-Free
+
+Lock-Free
+
+    4. Lock-Free (LF)
+
+Wait-Free
+
+    5. Wait-Free (WF)  
+
+    6. Wait-Free Bounded (WFB)
+
+    7. Wait-Free Population Oblivious (WFPO)
+
